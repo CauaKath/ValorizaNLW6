1c3a6dfef87129c8ebc1ac427af5ad0ac1591f23
Cauã Kath <cauakathdev@gmail.com>
2021-06-22 22:17:51 -0300

    Correção de erro

--- src/controllers/CreateUserController.ts
+++ src/controllers/CreateUserController.ts
@@ -4,13 +4,13 @@ import { CreateUserService } from "../service/CreateUserService";
 class CreateUserController {
 
     async handle(request: Request, response: Response) {
-        const { name, email, admin } = request.body;
+            const { name, email, admin } = request.body;
 
-        const createUserService = new CreateUserService();
+            const createUserService = new CreateUserService();
 
-        const user = await createUserService.execute({ name, email, admin });
+            const user = await createUserService.execute({ name, email, admin });
 
-        return response.json(user);
+            return response.json(user);
     }
 
 }
